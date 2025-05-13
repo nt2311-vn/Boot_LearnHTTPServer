225d0e43cf88af21b8c65b6abc232d7a94865493
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-13 22:36:36 +0700

    log output to serverlog

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 const app = express();
 const PORT = 8080;
+const middlewareLogResponses = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void => {
+  res.on("finish", () => {
+    const statusCode = res.statusCode;
 
-app.use(express.static("."));
+    if (statusCode >= 300) {
+      console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${statusCode}`);
+    }
+  });
+
+  next();
+};
+
+app.use(middlewareLogResponses);
+app.use("/app", express.static("./src/app"));
+
+const handlerReadiness = async (req: Request, res: Response): Promise<void> => {
+  res.set("Content-Type", "text");
+  res.status(200).send("OK");
+};
+
+app.get("/healthz", handlerReadiness);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
