d8fe38dad919aa9a4a8928623b01365278124d5a
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-14 21:57:44 +0700

    update config structure and write file index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import express, { NextFunction, Request, Response } from "express";
+import { config } from "./config";
 
 const app = express();
 const PORT = 8080;
+const middlewareMetricsInc = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  config.fileserverHits++;
+  next();
+};
+
 const middlewareLogResponses = (
   req: Request,
   res: Response,
@@ -18,7 +28,16 @@ const middlewareLogResponses = (
   next();
 };
 
-app.use(middlewareLogResponses);
+const handlerGetMetrics = async (_req: Request, res: Response) => {
+  res.set("Content-Type", "text");
+  res.status(200).send(`Hits: ${config.fileserverHits}`);
+};
+
+const handlerResetMetrics = async (_req: Request, _res: Response) => {
+  config.fileserverHits = 0;
+};
+
+app.use(middlewareLogResponses, middlewareMetricsInc);
 app.use("/app", express.static("./src/app"));
 
 const handlerReadiness = async (req: Request, res: Response): Promise<void> => {
@@ -27,6 +46,8 @@ const handlerReadiness = async (req: Request, res: Response): Promise<void> => {
 };
 
 app.get("/healthz", handlerReadiness);
+app.get("/metrics", handlerGetMetrics);
+app.get("/reset", handlerResetMetrics);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
