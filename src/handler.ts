ff81f800615af30baa4ed855de6e95282ed9ea83
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-16 21:15:46 +0700

    complete the handler for handle validate chirp POST

--- src/handler.ts
+++ src/handler.ts
@@ -23,4 +23,38 @@ const handlerReadiness = async (req: Request, res: Response): Promise<void> => {
   res.status(200).send("OK");
 };
 
-export { handlerResetMetrics, handlerReadiness, handlerGetMetrics };
+const handlerChirp = async (req: Request, res: Response) => {
+  type Chirp = {
+    body: string;
+  };
+
+  let body = "";
+  req.setEncoding("utf8");
+  req.on("data", (chunk: string) => {
+    body += chunk;
+  });
+
+  req.on("end", () => {
+    try {
+      const chirp: Chirp = JSON.parse(body);
+
+      if (chirp.body.length > 140) {
+        res.status(400).send({ error: "Chirp is too long" });
+      }
+      res.status(200).send({ valid: true });
+    } catch (err) {
+      res.status(400).send({ error: "Something went wrong" });
+    }
+  });
+
+  req.on("error", (err) => {
+    res.status(500).send({ error: "Something went wrong" });
+  });
+};
+
+export {
+  handlerResetMetrics,
+  handlerReadiness,
+  handlerGetMetrics,
+  handlerChirp,
+};
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import {
   handlerGetMetrics,
   handlerReadiness,
   handlerResetMetrics,
+  handlerChirp,
 } from "./handler.js";
 
 const app = express();
@@ -11,10 +12,12 @@ const PORT = 8080;
 
 app.use(middlewareLogResponses);
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
+app.use(express.json());
 
 app.get("/api/healthz", handlerReadiness);
 app.get("/admin/metrics", handlerGetMetrics);
 app.post("/admin/reset", handlerResetMetrics);
+app.post("/api/validate_chirp", handlerChirp);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
