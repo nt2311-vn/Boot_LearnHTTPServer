1d567b99b8820cdef941237cf9ca48ab049e046b
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-21 20:46:00 +0700

    write handler for getChirps

--- src/chirpController.ts
+++ src/chirpController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { BadRequestError } from "./app/customError.js";
-import { createChirp } from "./db/queries/chirps.js";
+import { createChirp, retrieveChirps } from "./db/queries/chirps.js";
 
 export const createChirpHandler = async (req: Request, res: Response) => {
   type ChirpRequest = {
@@ -21,4 +21,14 @@ export const createChirpHandler = async (req: Request, res: Response) => {
   }
 
   res.status(201).json(chirp);
+};
+
+export const getChirpsHandler = async (_: Request, res: Response) => {
+  const chirps = await retrieveChirps();
+
+  if (!chirps) {
+    throw new Error("Cannot create chirp");
+  }
+
+  res.status(200).json(chirps);
 };
