59a2717ef7a34d0ae2e663513bb467f26c373d07
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-21 21:01:06 +0700

    write controller to return chirp json

--- src/chirpController.ts
+++ src/chirpController.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
-import { BadRequestError } from "./app/customError.js";
-import { createChirp, retrieveChirps } from "./db/queries/chirps.js";
+import { BadRequestError, NotFoundError } from "./app/customError.js";
+import {
+  createChirp,
+  retrieveChirpById,
+  retrieveChirps,
+} from "./db/queries/chirps.js";
 
 export const createChirpHandler = async (req: Request, res: Response) => {
   type ChirpRequest = {
@@ -31,4 +35,20 @@ export const getChirpsHandler = async (_: Request, res: Response) => {
   }
 
   res.status(200).json(chirps);
+};
+
+export const getChirpByIdHandler = async (req: Request, res: Response) => {
+  const chirpID = req.params.chirpID;
+
+  if (!chirpID) {
+    throw new BadRequestError("Missing chirp id to retrieve");
+  }
+
+  const chirp = await retrieveChirpById(chirpID);
+
+  if (!chirp) {
+    throw new NotFoundError(`Cannot find chirp with id ${chirpID}`);
+  }
+
+  res.status(200).json(chirp);
 };
