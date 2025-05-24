e78812a9b360cf7ca6643c31d5a31b5618221b27
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-24 18:18:44 +0700

    write controller for update user

--- src/userController.ts
+++ src/userController.ts
@@ -1,7 +1,8 @@
-import { createUser } from "./db/queries/users.js";
+import { createUser, findUserByEmail } from "./db/queries/users.js";
 import { NextFunction, Request, Response } from "express";
 import { BadRequestError } from "./app/customError.js";
-import { hashPassword } from "./auth.js";
+import { getBearerToken, hashPassword, validateJWT } from "./auth.js";
+import { envOrThrow } from "./config.js";
 
 const createUserHandler = async (req: Request, res: Response) => {
   type parameters = {
@@ -26,6 +27,24 @@ const createUserHandler = async (req: Request, res: Response) => {
   }
 
   res.status(201).json(user);
+};
+
+const updateUserHandler = async (req: Request, res: Response) => {
+  type parameters = {
+    email: string;
+    password: string;
+  };
+
+  const params: parameters = req.body;
+  if (!params.email || !params.password) {
+    res.status(401).send("Missing required fields");
+  }
+
+  try {
+    const accessToken = getBearerToken(req);
+    const userId = validateJWT(accessToken, envOrThrow("SECRET"));
+    const user = findUserByEmail(params.email);
+  } catch (err) {}
 };
 
 export { createUserHandler };
